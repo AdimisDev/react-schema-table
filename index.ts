9a9dfa03966cf5020d8294f8bf183bfb1d459f72
adimis <aditya.mishra@eoncollective.com>
2024-04-30 03:03:00 +0530

    updates

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import "./src/index.css";
 
 export * from "./src/components/SchemaTable/DataTable/interface";
-export * from "./src/components/SchemaTable/EditableTable/interface";
+// export * from "./src/components/SchemaTable/EditableTable/interface";
 
 import { DataTable as SchemaDataTable } from "./src/components/SchemaTable/DataTable/DataTable";
-import { EditableTable as SchemaEditableTable } from "./src/components/SchemaTable/EditableTable/EditableTable";
+// import { EditableTable as SchemaEditableTable } from "./src/components/SchemaTable/EditableTable/EditableTable";
 
-export { SchemaDataTable, SchemaEditableTable };
+export {
+  SchemaDataTable,
+  // SchemaEditableTable
+};
