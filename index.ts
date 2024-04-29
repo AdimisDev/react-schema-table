d65c37d9c7ed96ff875b0046f4efa55be1e42afc
adimis <aditya.mishra@eoncollective.com>
2024-04-30 02:42:27 +0530

    built test success

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "@/components/SchemaTable/DataTable/interface";
+import "./src/index.css";
+
+import { DataTable as SchemaDataTable } from "@/components/SchemaTable/DataTable/DataTable";
+
+export default SchemaDataTable;
