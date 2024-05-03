7cd11fd19ae92452cf0db2d8c74c919175b2eb34
adimis <aditya.mishra@eoncollective.com>
2024-05-04 01:48:33 +0530

    nextjs support tested on nextjs 14 and test successful

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
+import "./src/index.css";
+
 import { useKeyPress } from "./src/hooks/useKeyPress";
-export * from "./src/components/SchemaTable/index";
-export { useKeyPress };
+import {
+  SchemaDataTable,
+  DataTableColumnHeader,
+  DataTablePagination,
+  DataTableViewOptions,
+  DataTableActionCell,
+  DataTableFilter,
+  DataTableSearch,
+} from "./src/components/SchemaTable/index";
+
+export * from "./src/components/SchemaTable/interface";
+export {
+  useKeyPress,
+  SchemaDataTable,
+  DataTableColumnHeader,
+  DataTablePagination,
+  DataTableViewOptions,
+  DataTableActionCell,
+  DataTableFilter,
+  DataTableSearch,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 export default defineConfig({
   resolve: {
@@ -12,20 +12,26 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./index.ts"),
-      name: "Adimis|React-Schema-Form",
-      fileName: (format) => `index.${format}.js`,
+      name: 'AdimisReactSchemaTable',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
     },
     sourcemap: true,
-    emptyOutDir: true,
+    emptyOutDir: true
   },
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true
+    })
+  ]
 });
