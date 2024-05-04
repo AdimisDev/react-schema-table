afa4eb17e83e5c45cc60ecff223d4cfbf10e9992
adimis <aditya.mishra@eoncollective.com>
2024-05-04 22:03:26 +0530

    storage nextjs fix

--- src/components/SchemaTable/context/ThemeProvider.tsx
+++ src/components/SchemaTable/context/ThemeProvider.tsx
@@ -1,6 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { Theme, ThemeColors, ThemeProviderProps, ThemeProviderState } from "../interface";
-
+import {
+  Theme,
+  ThemeColors,
+  ThemeProviderProps,
+  ThemeProviderState,
+} from "../interface";
 
 const initialState: ThemeProviderState = {
   theme: "system",
@@ -16,9 +20,16 @@ export function ThemeProvider({
   themeColors,
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  );
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const storedTheme = localStorage.getItem(storageKey) as Theme;
+      if (storedTheme) {
+        setTheme(storedTheme);
+      }
+    }
+  }, [storageKey]);
 
   useEffect(() => {
     const root = window.document.documentElement;
